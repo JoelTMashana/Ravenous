dd9ffa8a7719ced1afaa6ab371e05053c05c54d6
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2021-01-08 09:50:32 +0000

    API key set and fetch whatwg installed

---
+++ src/util/Yelp.js
@@ -0,0 +1,9 @@
+const apiKey = o-M3jFXTz-jIPzeico6htaCaoWQu4gKuN22OPcOroMKzOc66uE1EZJ2hHDmksOmZr4J8tOZh9h0ii280McIwjvSicDUxM5LnQ7nzX5QMqWUzQP7gWTZuuyqM9iX4X3Yx;
+
+//stores functionality needed to interact with API
+const Yelp = {};
+
+//Retrieves search results from API
+search(term, location, sortBy) {
+  return fetch();
+}
